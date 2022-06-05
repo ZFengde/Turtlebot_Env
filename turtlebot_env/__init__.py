a80e61769fa506f2ca1a6d4f198b9c938e7f32d9
feng <feng.1.zhang@kcl.ac.uk>
2022-06-05 12:10:50 +0100

    comments of envs

--- turtlebot_env/__init__.py
+++ turtlebot_env/__init__.py
@@ -2,20 +2,25 @@
 
 from gym.envs.registration import register
 
+# fixed target at x, y = (1.5, 1.5)
 register(
     id='Turtlebot-v0', 
     entry_point='turtlebot_env.envs:TurtleBotEnv_Simple'
 )
+
+# random target with distance related reward
 register(
     id='Turtlebot-v1', 
     entry_point='turtlebot_env.envs:TurtleBotEnv'
 )
 
+# random target with sparse reward
 register(
     id='Turtlebot-v2', 
     entry_point='turtlebot_env.envs:TurtleBotEnv_Sparse'
 )
 
+# parallel version environment, not yet finished
 register(
     id='Turtlebot-v3', 
     entry_point='turtlebot_env.envs:TurtleBotEnv_Parallel'
