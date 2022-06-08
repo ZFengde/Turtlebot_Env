bec8db69af986c56ba3e2e19d4e072fb2fd90342
feng <feng.1.zhang@kcl.ac.uk>
2022-06-08 12:59:31 +0100

    init change

--- turtlebot_env/__init__.py
+++ turtlebot_env/__init__.py
@@ -20,16 +20,9 @@ register(
     entry_point='turtlebot_env.envs:TurtleBotEnv_Sparse'
 )
 
-# parallel version environment, not yet finished
-# TODO
-register(
-    id='Turtlebot-v3', 
-    entry_point='turtlebot_env.envs:TurtleBotEnv_Parallel'
-)
-
 # random target with distance related reward, but speed up and break real features of the robot
 # WORKING RIGHT NOW
 register(
-    id='Turtlebot-v4', 
+    id='Turtlebot-v3', 
     entry_point='turtlebot_env.envs:TurtleBotEnv_Speedup'
 )
