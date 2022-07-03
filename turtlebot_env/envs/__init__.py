a110c3775d89b5d3b596ad63f33d0e6e7ae8311c
feng <feng.1.zhang@kcl.ac.uk>
2022-07-03 15:17:39 +0100

    for test

--- turtlebot_env/envs/__init__.py
+++ turtlebot_env/envs/__init__.py
@@ -2,3 +2,4 @@
 
 from turtlebot_env.envs.turtlebot_env import TurtleBotEnv
 from turtlebot_env.envs.turtlebot_env_fuzzy import TurtleBotEnv_Fuzzy_Reward
+from turtlebot_env.envs.turtlebot_env_fuzzy_2 import TurtleBotEnv_Fuzzy_2_Reward
