e6e258a64b8351b0ba16c22fc03302e6729d8eab
Feng Zhang <feng.1.zhang@kcl.ac.uk>
2023-02-27 13:36:52 +0000

    env update

--- turtlebot_env/envs/turtlebot_constrained_reward.py
+++ turtlebot_env/envs/turtlebot_constrained_reward.py
@@ -103,10 +103,10 @@ class TurtleBotEnv_Constrained_Reward(gym.Env):
         self.info['cost'] = 0
         dist_robot_obstalces = np.linalg.norm((pos - self.obstacle_bases), axis=1)
         for i in range(len(dist_robot_obstalces)):
-            if dist_robot_obstalces[i] < 0.3:
+            if dist_robot_obstalces[i] < 0.5:
                 self.info['cost'] += 0.1
             elif dist_robot_obstalces[i] < 0.5:
-                reward -= 10 * (dist_robot_obstalces[i] - self.prev_dist_robot_obstalces[i]) / (dist_robot_obstalces[i] + 1e-7)
+                reward -= 30 * (dist_robot_obstalces[i] - self.prev_dist_robot_obstalces[i]) / (dist_robot_obstalces[i] + 1e-7)
 
         self.prev_dist_robot_obstalces = dist_robot_obstalces
         # obs: robot [: 6], target [6: 8], obstacles [8: ]
