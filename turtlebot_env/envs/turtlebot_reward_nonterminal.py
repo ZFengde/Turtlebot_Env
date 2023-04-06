5aad281bc11f821effc575851167e6b9eadc231b
Feng Zhang <feng.1.zhang@kcl.ac.uk>
2023-04-06 17:53:29 +0100

    update

--- turtlebot_env/envs/turtlebot_reward_nonterminal.py
+++ turtlebot_env/envs/turtlebot_reward_nonterminal.py
@@ -78,12 +78,10 @@ class TurtleBotEnv_Reward_Nonterminal(gym.Env):
 
         for i in range(len(dist_robot_obstalces)):
             if dist_robot_obstalces[i] < 0.27:
-                reward = -0.3
+                reward = -0.2
                 self.info['Collision'] = True
-            # elif dist_robot_obstalces[i] < 0.4:
-            #     reward -= 60 * (self.prev_dist_robot_obstalces[i] - dist_robot_obstalces[i])
-            elif dist_robot_obstalces[i] < 0.6:
-                reward -= 40 * (self.prev_dist_robot_obstalces[i] - dist_robot_obstalces[i])
+            elif dist_robot_obstalces[i] < 0.4:
+                reward -= 0.1
 
         self.prev_dist_to_target = dist_to_target
         self.prev_dist_robot_obstalces = dist_robot_obstalces
